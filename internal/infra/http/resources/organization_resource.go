eb43a3be6a213eb2ca443a5b9b94544bb90a1a5c
BohdanBoriak <lightraven72@gmail.com>
2026-05-06 15:21:47 +0300

    Add: Find List

--- internal/infra/http/resources/organization_resource.go
+++ internal/infra/http/resources/organization_resource.go
@@ -25,3 +25,11 @@ func (d OrganizationDto) DomainToDto(o domain.Organization) OrganizationDto {
 		Lon:         o.Lon,
 	}
 }
+
+func (d OrganizationDto) DomainToDtoCollection(orgs []domain.Organization) []OrganizationDto {
+	orgsDto := make([]OrganizationDto, len(orgs))
+	for i := range orgs {
+		orgsDto[i] = d.DomainToDto(orgs[i])
+	}
+	return orgsDto
+}
